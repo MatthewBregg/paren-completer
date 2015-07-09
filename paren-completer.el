b2ed8d9ef3fe5c05f03c8d2d1fa0691a170fb169
Matthew Bregg <matthewbregg610@ufl.edu>
2015-07-08 23:45:44 -0400

    Updated readme

--- paren-completer.el
+++ paren-completer.el
@@ -25,8 +25,7 @@
 ;;; Commentary:
 
 ;; Provides 4 functions to generically auto-complete delimiters.
-;; Avoids use of syntax table, instead relies on user defined lists.
-;; (The syntax table didn't seem to have everything I wanted, like <> brackets in c++.)
+;; Avoids use of syntax table, instead relies on user defined lists.(except for strings and comments)
 ;; See readme.org
 
 ;;; Code:
